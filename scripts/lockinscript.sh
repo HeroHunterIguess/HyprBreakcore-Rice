a20447d5ddb303b493e3793f0ed595dcb1a1f5be
Raylee <abramscarter1999@gmail.com>
2026-05-08 17:39:18 -0400

    Create lockinscript.sh

---
+++ scripts/lockinscript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+LOCKFILE="/tmp/lockdown_start"
+PIDFILE="/tmp/lockdown_pid"
+
+# check if another instance is already running
+if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE") 2>/dev/null; then
+    notify-send "Already locked in!"
+    exit 0
+fi
+
+# save current PID and timestamp
+echo $$ > "$PIDFILE"
+date +%s > "$LOCKFILE"
+
+notify-send "LOCK IN" "No more distractions!"
+
+
+while true; do
+    pkill discord
+    pkill vesktop
+    pkill steam
+    pkill lutris
+    pkill heroic
+    pkill firefox
+    pkill -f AppRun.wrapped
+    sleep 1
+done
