b4bc1a7992af47f0a61b0c5ec5f210e839e671a8
Raylee <abramscarter1999@gmail.com>
2026-06-01 07:46:41 -0400

    remove old unused alias in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,9 +10,6 @@ end
 # calculator alias'
 alias calc='cd "/home/herohunter/Everything/Coding/rust projects/" && rustc FINALcalculator.rs && "/home/herohunter/Everything/Coding/rust projects/"FINALcalculator'
 
-alias pythoncalc='python -u "/home/herohunter/Everything/Coding/better coding lol/new pc coding/calculator.py"'
-
-
 
 # cd into folder for waves game files
 alias cdw='cd ~/Everything/godot\ projects/waves'
